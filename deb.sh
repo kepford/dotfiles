6f18cd267296c634e88a66247c92d7756170a008
kepford <bob@bobkepford.com>
2020-07-16 18:40:32 -0700

    Adds debian packages and conditions for linux
    - Doing this for the RaspberryPi

---
+++ deb.sh
@@ -0,0 +1,26 @@
+sudo apt-get update
+
+sudo apt-get -y install ansible
+sudo apt-get -y install composer
+sudo apt-get -y install ctags
+sudo apt-get -y install fzf
+sudo apt-get -y install git-extras
+sudo apt-get -y install git-flow
+sudo apt-get -y install htop
+sudo apt-get -y install httpie
+sudo apt-get -y install jq
+sudo apt-get -y install mosh
+sudo apt-get -y install nmap
+sudo apt-get -y install peco
+sudo apt-get -y install php
+sudo apt-get -y install php-curl
+sudo apt-get -y install php-mbstring
+sudo apt-get -y install php-simplexml
+sudo apt-get -y install php-zip
+sudo apt-get -y install ruby
+sudo apt-get -y install silversearcher-ag
+sudo apt-get -y install tig
+sudo apt-get -y install tmux
+sudo apt-get -y install tmuxinator
+sudo apt-get -y install tree
+sudo apt-get -y install vim-nox
