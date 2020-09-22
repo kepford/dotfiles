2b8df592390dfa33580066bc9342e345d8eac744
kepford <bob@bobkepford.com>
2020-09-22 09:34:45 -0700

    Adds setup scripts for nvm and git-notpushed

---
+++ setup/git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s ~/bin/dotfiles/git/git-notpushed ~/bin/git-notpushed
+
---
+++ setup/nvm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
