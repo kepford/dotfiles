c14d11f9cd2d9282e481a2721bd29eeda5df1125
kepford <bob@bobkepford.com>
2020-09-22 15:32:59 -0700

    Adds misc.sh setup file

---
+++ setup/misc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# plist files
+# Use this tool to view plist files in the terminal: plutil -p filename
+
+ln -s ~/bin/dotfiles/jumpcut/net.sf.Jumpcut.plist ~/Library/Preferences/net.sf.Jumpcut.plist
