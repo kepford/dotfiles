1847d9ec824eb14f6a657f83cfaf1bef9e7d4a25
kepford <bob@bobkepford.com>
2020-10-09 14:45:10 -0400

    Hammerspoon: Rewrites app launching and fixes for global shortcuts
    - Fixes global keyboard shortcuts for Alfred
    - Fixes global Fantastical

---
+++ hammerspoon/brave.lua
@@ -0,0 +1,72 @@
+-- BRAVE
+--
+-- Some utility functions for controlling Brave Browser.
+-- Probably would work super similarly on Chrome and Safari, or any webkit
+-- browser.
+--
+-- NOTE: May require you enable View -> Developer -> Allow Javascript from
+-- Apple Events in Brave's menu.
+
+local fn   = require('hs.fnutils')
+local module = {}
+
+module.start = function(config_table)
+  module.config = config_table
+end
+
+module.jump = function(url)
+  hs.osascript.javascript([[
+  (function() {
+    var brave = Application('Brave');
+    brave.activate();
+
+    for (win of brave.windows()) {
+      var tabIndex =
+        win.tabs().findIndex(tab => tab.url().match(/]] .. url .. [[/));
+
+      if (tabIndex != -1) {
+        win.activeTabIndex = (tabIndex + 1);
+        win.index = 1;
+      }
+    }
+  })();
+  ]])
+end
+
+module.urlsTaggedWith = function(tag)
+  return fn.filter(module.config.domains, function(domain)
+    return domain.tags and fn.contains(domain.tags, tag)
+  end)
+end
+
+module.launch = function(list)
+  fn.map(list, function(tag)
+    fn.map(module.urlsTaggedWith(tag), function(site)
+      hs.urlevent.openURL("http://" .. site.url)
+    end)
+  end)
+end
+
+module.kill = function(list)
+  fn.map(list, function(tag)
+    fn.map(module.urlsTaggedWith(tag), function(site) module.killTabsByDomain(site.url) end)
+  end)
+end
+
+module.killTabsByDomain = function(domain)
+  hs.osascript.javascript([[
+  (function() {
+    var brave = Application('Brave');
+
+    for (win of brave.windows()) {
+      for (tab of win.tabs()) {
+        if (tab.url().match(/]] .. string.gsub(domain, '/', '\\/') .. [[/)) {
+          tab.close()
+        }
+      }
+    }
+  })();
+  ]])
+end
+
+return module
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,79 +1,66 @@
--- Modified from https://gist.github.com/ttscoff/cce98a711b5476166792d5e6f1ac5907
-
--- A global variable for the Hyper Mode
-k = hs.hotkey.modal.new({}, "F17")
-
--- Trigger existing hyper key shortcuts
-
-k:bind({}, 'm', nil, function() hs.eventtap.keyStroke({"cmd","alt","shift","ctrl"}, 'm') end)
-
--- OR build your own
-
-launch = function(appname)
-  hs.application.launchOrFocus(appname)
-  k.triggered = true
-end
-
--- Single keybinding for app launch
-singleapps = {
-  {'q', 'MailMate'},
-  {'w', 'OmniFocus'},
-  {'f', 'Firefox'},
-  {'s', 'Slack'},
-  {'t', 'iTerm'},
-  {'c', 'Fantastical'},
-  {'z', 'zoom.us'}
+-- https://github.com/evantravers/hammerspoon-config
+config = {}
+config.applications = {
+  ['Alfred'] = {
+    bundleID = 'com.runningwithcrayons.Alfred',
+    local_bindings = {'s', 'space', 's'}
+  },
+  ['Jumpcut'] = {
+    bundleID = 'net.sf.Jumpcut',
+    local_bindings = {',', '\\'}
+  },
+  ['Brave'] = {
+    bundleID = 'com.brave.browser',
+    hyper_key = 'b',
+    preferred_display = 1
+  },
+  ['Firefox'] = {
+    bundleID = 'org.mozilla.firefox',
+    hyper_key = 'k',
+    preferred_display = 1
+  },
+  ['Dash'] = {
+    bundleID = 'com.kapeli.dashdoc',
+    hyper_key = 'h',
+    tags = {'#coding'}
+  },
+  ['iTerm'] = {
+    bundleID = 'com.googlecode.iterm2',
+    hyper_key = 't',
+    preferred_display = 2
+  },
+  ['Fantastical'] = {
+    bundleID = 'com.flexibits.fantastical2.mac',
+    hyper_key = 'c',
+    local_bindings = {']'},
+    tags = {'#planning', '#review', '#calendar'},
+    preferred_display = 2,
+  },
+  ['Finder'] = {
+    bundleID = 'com.apple.finder',
+    hyper_key = 'f'
+  },
+  ['Drafts'] = {
+    bundleID = 'com.agiletortoise.Drafts-OSX',
+    hyper_key ='d',
+    tags = {'#review', '#writing', '#research'},
+    local_bindings = {'x', ';'}
+  },
+  ['OmniFocus'] = {
+    bundleID = 'com.omnigroup.OmniFocus3.MacAppStore',
+    tags = {'#planning'},
+    hyper_key = 'w',
+    preferred_display = 1
+  },
+  ['MailMate'] = {
+    bundleID = 'com.freron.MailMate',
+    hyper_key = 'q',
+    tags = {'#communication'}
+  }
 }
 
-for i, app in ipairs(singleapps) do
-  k:bind({}, app[1], function() launch(app[2]); k:exit(); end)
-end
+hyper = require('hyper')
+hyper.start(config)
 
--- Sequential keybindings, e.g. Hyper-a,f for Finder
-a = hs.hotkey.modal.new({}, "F16")
-apps = {
-  {'t', 'iTerm'},
-  {'f', 'Finder'},
-}
-for i, app in ipairs(apps) do
-  a:bind({}, app[1], function() launch(app[2]); a:exit(); end)
-end
-
-pressedA = function() a:enter() end
-releasedA = function() end
-k:bind({}, 'a', nil, pressedA, releasedA)
-
--- Shortcut to reload config
-
-ofun = function()
-  hs.reload()
-  hs.alert.show("Config loaded")
-  k.triggered = true
-end
-k:bind({}, 'o', nil, ofun)
-
--- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k.triggered = false
-  k:enter()
-end
-
--- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
---   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
-  k:exit()
-  if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
-  end
-end
-
--- Bind the Hyper key
-f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
-
--- HYPER+L: Open news.google.com in the default browser
--- lfun = function()
---   news = "app = Application.currentApplication(); app.includeStandardAdditions = true; app.doShellScript('open http://news.google.com')"
---   hs.osascript.javascript(news)
---   k.triggered = true
--- end
--- k:bind('', 'l', nil, lfun)
+local brave = require('brave')
+      brave.start(config)
