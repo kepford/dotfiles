ec7d85fe435b4188f385424b99696cb6948ef30b
kepford <bob@bobkepford.com>
2023-09-06 10:21:55 -0700

    Adds local configuration

---
+++ .local/bin/sync-audiobooks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Sync audio books from Open Audible to Audiobookshelf app
+
+# -a uses archive mode which preserves timestamps permissions
+rsync -avzh --log-file=/tmp/sync-audiobooks.log --info=stats2 /home/kepford/OpenAudible/books/* containerhost:/home/kepford/appdata/audiobookshelf/audiobooks | tee /tmp/sync-audiobooks-output
+notify-send "Sync Audiobooks completed!" "$(tail -15 /tmp/sync-audiobooks-output)"
+
