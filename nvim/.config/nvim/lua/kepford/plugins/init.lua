b5d8aca5e805579af4e6b3fadd27c1dafdf9453c
kepford <bob@bobkepford.com>
2024-03-15 13:22:05 -0700

    Removes packer

--- nvim/.config/nvim/lua/kepford/plugins/init.lua
+++ nvim/.config/nvim/lua/kepford/plugins/init.lua
@@ -1,18 +1,20 @@
 return {
-  "wincent/loupe", -- Enhances search in Vim. consider removing
-  "mbbill/undotree",
-  "tpope/vim-commentary", -- comment/uncomment lines with gcc or gc in visual mode
-  "nvim-treesitter/playground",
-  "nvim-treesitter/nvim-treesitter-context",
-  "tpope/vim-eunuch", -- Vim sugar for UNIX shell commands that need it the most.
-  "tpope/vim-vinegar",
-  "tpope/vim-unimpaired",
-  "christoomey/vim-tmux-navigator",
-  "tpope/vim-surround",
-  "tpope/vim-rsi", -- Readline style insertion.
-  "tpope/vim-repeat",
-  "pbrisbin/vim-mkdir",
-  "machakann/vim-highlightedyank",
-  'airblade/vim-gitgutter',
-  'dkarter/bullets.vim' -- Bullets.vim is a Vim/NeoVim plugin for automated bullet lists.
+	"wincent/loupe", -- Enhances search in Vim. consider removing
+	"mbbill/undotree",
+	"tpope/vim-commentary", -- comment/uncomment lines with gcc or gc in visual mode
+	"nvim-treesitter/playground",
+	"nvim-treesitter/nvim-treesitter-context",
+	"tpope/vim-eunuch", -- Vim sugar for UNIX shell commands that need it the most.
+	"tpope/vim-vinegar",
+	"tpope/vim-unimpaired",
+	"christoomey/vim-tmux-navigator",
+	"tpope/vim-surround",
+	"tpope/vim-rsi", -- Readline style insertion.
+	"tpope/vim-repeat",
+	"pbrisbin/vim-mkdir",
+	"machakann/vim-highlightedyank",
+	"airblade/vim-gitgutter",
+	"dkarter/bullets.vim", -- Bullets.vim is a Vim/NeoVim plugin for automated bullet lists.
+	"stevearc/conform.nvim", -- Formatting
+	"mfussenegger/nvim-lint", -- Linting
 }
