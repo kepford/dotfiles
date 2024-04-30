87c918655cc893d7a4356b53d065fd41fb2131c2
kepford <bob@bobkepford.com>
2024-04-29 17:45:46 -0700

    Adds crontab and Neovim fixes
    Deletes lazy lock file
    Fixes plugins init
    Spelling updates

--- nvim/.config/nvim/lua/kepford/plugins/init.lua
+++ nvim/.config/nvim/lua/kepford/plugins/init.lua
@@ -1,20 +1,27 @@
 return {
-	"wincent/loupe", -- Enhances search in Vim. consider removing
-	"mbbill/undotree",
-	"tpope/vim-commentary", -- comment/uncomment lines with gcc or gc in visual mode
-	"nvim-treesitter/playground",
-	"nvim-treesitter/nvim-treesitter-context",
-	"tpope/vim-eunuch", -- Vim sugar for UNIX shell commands that need it the most.
-	"tpope/vim-vinegar",
-	"tpope/vim-unimpaired",
-	"christoomey/vim-tmux-navigator",
-	"tpope/vim-surround",
-	"tpope/vim-rsi", -- Readline style insertion.
-	"tpope/vim-repeat",
-	"pbrisbin/vim-mkdir",
-	"machakann/vim-highlightedyank",
-	"airblade/vim-gitgutter",
-	"dkarter/bullets.vim", -- Bullets.vim is a Vim/NeoVim plugin for automated bullet lists.
-	"stevearc/conform.nvim", -- Formatting
-	"mfussenegger/nvim-lint", -- Linting
+  "airblade/vim-gitgutter",
+  "christoomey/vim-tmux-navigator",
+  "dkarter/bullets.vim", -- Bullets.vim is a Vim/NeoVim plugin for automated bullet lists.
+  "machakann/vim-highlightedyank",
+  "mbbill/undotree",
+  "mfussenegger/nvim-lint", -- Linting
+  "nvim-treesitter/nvim-treesitter-context",
+  "nvim-treesitter/playground",
+  "pbrisbin/vim-mkdir",
+  "stevearc/conform.nvim", -- Formatting
+  "tpope/vim-commentary", -- comment/uncomment lines with gcc or gc in visual mode
+  "tpope/vim-eunuch", -- Vim sugar for UNIX shell commands that need it the most.
+  "tpope/vim-repeat",
+  "tpope/vim-rsi", -- Readline style insertion.
+  "tpope/vim-surround",
+  "tpope/vim-unimpaired",
+  "tpope/vim-vinegar",
+  "wincent/loupe", -- Enhances search in Vim. consider removing
+  'airblade/vim-gitgutter',
+  'dkarter/bullets.vim', -- Bullets.vim is a Vim/NeoVim plugin for automated bullet lists.
+  'mfussenegger/nvim-dap', -- DAP (Debug Adapter Protocol) debugging for Drupal :)
+  'nvim-telescope/telescope-dap.nvim', -- Integration for nvim-dap with telescope.nvim. Overriding dap internal ui, so running any dap command, which makes use of the internal ui, will result in a telescope prompt.
+  'rcarriga/nvim-dap-ui',
+  'theHamsta/nvim-dap-virtual-text', -- adds virtual text support to nvim-dap. nvim-treesitter is used to find variable definitions.
+  -- use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} 
 }
