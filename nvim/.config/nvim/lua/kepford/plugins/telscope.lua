a8a1ded95565128fe08249073ed76cd3d1beab12
kepford <bob@bobkepford.com>
2024-06-05 12:44:42 -0700

    Adds oil.nvim

--- nvim/.config/nvim/lua/kepford/plugins/telscope.lua
+++ nvim/.config/nvim/lua/kepford/plugins/telscope.lua
@@ -36,8 +36,8 @@ return {
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set('n', '<leader>fg', "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in git" })
-    keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find file in buffers" })
+    keymap.set("n", '<leader>fg', "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in git" })
+    keymap.set("n", '<leader>fb', "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find file in buffers" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
   end,
