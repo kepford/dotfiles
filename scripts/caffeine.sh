6f4df23654ee8d1df96ca3c8cb65e63ec7ee57db
kepford <bob@bobkepford.com>
2024-06-25 07:35:43 -0700

    Updates caffeine

--- scripts/caffeine.sh
+++ scripts/caffeine.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 status=`xset -q | grep 'DPMS is' | awk '{ print $3 }'`
 if [ $status == 'Enabled' ]; then
+    xset s off && \
     xset -dpms && \
     dunstify 'Screen suspend is disabled.'
 else
-   	    xset +dpms && \
+    xset +dpms && \
+    xset s 900 && \
     dunstify 'Screen suspend is enabled.'
 fi
