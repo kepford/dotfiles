a781655c5ae67c810873f93f8cfe691a34a8383d
kepford <bob@bobkepford.com>
2024-06-27 17:11:51 -0700

    Adds tmux config for Yazi

--- setup/tmux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Installs Tmux plugin manager (tpm)
-# * Press `prefix + I` (capital i, as in Install) to fetch the plugins.
-# * `prefix + U` updates plugins
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
