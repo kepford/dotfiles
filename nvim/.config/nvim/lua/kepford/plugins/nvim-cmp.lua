e35a30ef6bf3b05cf212e9d65dd145bfea312c10
kepford <bob@bobkepford.com>
2024-07-10 16:05:51 -0700

    Adds noice and hrsh7th/cmp-cmdline

--- nvim/.config/nvim/lua/kepford/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/kepford/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-cmdline",
     "L3MON4D3/LuaSnip", -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for auto-completion
     "rafamadriz/friendly-snippets", -- useful snippets
