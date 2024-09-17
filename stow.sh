6f7b4dccf20225e001bfcb841c7a5de67d89fa45
kepford <bob@bobkepford.com>
2024-09-17 11:49:36 -0700

    Adds bc alias and sway

--- stow.sh
+++ stow.sh
@@ -1,8 +1,21 @@
 rm ~/.bashrc
 rm ~/.bash_profile
+stow -v -R -t ~ autorandr
 stow -v -R -t ~ bash
 stow -v -R -t ~ git
 stow -v -R -t ~ nvim
 stow -v -R -t ~ tmux
 stow -v -R -t ~ readline
 stow -v -R -t ~ alacritty
+stow -v -R -t ~ kitty
+stow -v -R -t ~ flameshot
+stow -v -R -t ~ i3
+stow -v -R -t ~ i3status-rust
+stow -v -R -t ~ picom
+stow -v -R -t ~ readline
+stow -v -R -t ~ rofi
+stow -v -R -t ~ tig
+stow -v -R -t ~ x
+stow -v -R -t ~ xdg
+stow -v -R -t ~ yazi
+stow -v -R -t ~ zathura
