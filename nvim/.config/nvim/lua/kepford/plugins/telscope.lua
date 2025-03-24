8b41f012520d48ab0b8b052b15283be457d55ec1
kepford <bob@bobkepford.com>
2025-03-24 15:22:33 -0700

    Updates spacing

--- nvim/.config/nvim/lua/kepford/plugins/telscope.lua
+++ nvim/.config/nvim/lua/kepford/plugins/telscope.lua
@@ -1,39 +1,44 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-    local builtin = require('telescope.builtin')
-    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
-    keymap.set("n", '<leader>fg', builtin.git_files, { desc = "Fuzzy find files in git" })
-    keymap.set("n", '<leader>fb', builtin.buffers, { desc = "Fuzzy find file in buffers" })
-    keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in current working directory" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in current working directory" })
-    keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find string in help"})
-  end,
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+		local builtin = require("telescope.builtin")
+		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Fuzzy find files in git" })
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Fuzzy find file in buffers" })
+		keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in current working directory" })
+		keymap.set(
+			"n",
+			"<leader>fc",
+			"<cmd>Telescope grep_string<cr>",
+			{ desc = "Find string under cursor in current working directory" }
+		)
+		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find string in help" })
+	end,
 }
