4147e21b92a6ddda7b70a14384434f110ffa6564
kepford <bob@bobkepford.com>
2025-05-06 08:18:41 -0700

    feat: ✨ Added typescript snippets and nvim-dap-ui plugin

---
+++ nvim/.config/nvim/lua/kepford/plugins/nvim-dap-ui.lua
@@ -0,0 +1,18 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = "mfussenegger/nvim-dap",
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dapui.setup()
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+	end,
+}
