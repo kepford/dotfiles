86748e8b243bfcc5879a002c3b2885fb548d4431
kepford <bob@bobkepford.com>
2025-08-26 11:16:58 -0700

    Disables nvim-notify in noice nvim
    I like noice but not the nvim-notify part.

--- nvim/.config/nvim/lua/kepford/plugins/noice.lua
+++ nvim/.config/nvim/lua/kepford/plugins/noice.lua
@@ -10,7 +10,7 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
 	},
 	config = function()
 		require("noice").setup({
